897b7f7af4279864205a0df2d23ecce59adaa5c1
Your Name <ssjbmew@gmail.com>
2023-12-01 19:12:52 +0100

    front accueil

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-  // ignore_for_file: library_private_types_in_public_api, prefer_const_constructors, sort_child_properties_last, duplicate_ignore
-
- // ignore_for_file: library_private_types_in_public_api, prefer_const_constructors, sort_child_properties_last
+  // ignore_for_file: library_private_types_in_public_api, prefer_const_constructors, sort_child_properties_last, duplicate_ignore, deprecated_member_use, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'seances.dart';
@@ -103,8 +101,163 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Accueil Screen'),
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Center(
+            child: Text(
+              'Exercice suivant',
+              style: Theme.of(context).textTheme.headline6!.copyWith(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.only(top: 16.0), 
+            elevation: 0, 
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                      Colors.blue.withOpacity(0.9), 
+                      BlendMode.overlay,
+                    ),
+                    child: Image.network(
+                      'https://www.expo2020dubai.com/-/media/expo2020/2021/experiences/sport-and-fitness/fitness-hero-1920x1080.png',
+                      height: 150,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(height: 3),
+                      Text(
+                        'Course à pieds\n30km - Durée estimée : 1h15',
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          
+          SizedBox(height: 27), 
+  Column(
+  crossAxisAlignment: CrossAxisAlignment.stretch,
+  children: [
+    Text(
+      'Activités récentes',
+      style: Theme.of(context).textTheme.headline6!.copyWith(fontSize: 18, fontWeight: FontWeight.bold),
+    ),
+    SizedBox(height: 10),
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListTile(
+        leading: Image.network(
+          'https://www.expo2020dubai.com/-/media/expo2020/2021/experiences/sport-and-fitness/fitness-hero-1920x1080.png',
+          height: 80,
+          fit: BoxFit.cover,
+        ),
+        title: Text(
+          'Titre de l\'activité',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+    ),
+    Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: LinearProgressIndicator(
+        value: 0.50,
+        backgroundColor: Colors.grey,
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+      ),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Progression : 10/20',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          InkWell(
+            onTap: () {
+              // Navigator.push();
+            },
+            child: Row(
+              children: [
+                Text(
+                  'Continuer',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Icon(Icons.arrow_forward),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
+        Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListTile(
+        leading: Image.network(
+          'https://www.expo2020dubai.com/-/media/expo2020/2021/experiences/sport-and-fitness/fitness-hero-1920x1080.png',
+          height: 80,
+          fit: BoxFit.cover,
+        ),
+        title: Text(
+          'Titre de l\'activité',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+    ),
+    Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: LinearProgressIndicator(
+        value: 0.50,
+        backgroundColor: Colors.grey,
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+      ),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Progression : 10/20',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          InkWell(
+            onTap: () {
+              // Navigator.push();
+            },
+            child: Row(
+              children: [
+                Text(
+                  'Continuer',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Icon(Icons.arrow_forward),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
+  ],
+),
+        ],
+      ),
     );
   }
 }
